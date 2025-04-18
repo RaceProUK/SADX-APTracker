943d396d0abfd7a8b21ca2a650261d93b780fc1b
RaceProUK <raceprouk@hotmail.co.uk>
2025-04-18 21:27:45 +0100

    Missions visibility implemented

--- Generator/Utilities/LocationGenerator.Missions.cs
+++ Generator/Utilities/LocationGenerator.Missions.cs
@@ -13,6 +13,15 @@ internal static partial class LocationGenerator
     {
         static int CalculateX(int number) => 56 * ((number - 1) % 10) + 38;
         static int CalculateY(int number) => 44 * ((number - 1) / 10) + 30;
+        static string GetMissionCharacter(int number) => number switch
+        {
+            4 or 10 or 16 or 24 or 31 or 37 or 47 or 54 => "Tails",
+            5 or 12 or 25 or 26 or 32 or 38 or 48 or 56 or 59 => "Knuckles",
+            6 or 18 or 19 or 43 or 50 => "Amy",
+            7 or 21 or 39 or 42 or 51 => "Gamma",
+            8 or 14 or 22 or 29 or 35 or 44 or 52 or 60 => "Big",
+            _ => "Sonic"
+        };
 
         var missions = from entry in dict
                        where entry.Key >= MissionsStart && entry.Key < MissionsEnd
@@ -21,7 +30,8 @@ internal static partial class LocationGenerator
                        let y = CalculateY(number)
                        select new Location(entry.Value,
                                            [new MapLocation("missions", x, y, MissionsIconSize, BorderThickness)],
-                                           [new Section(Constants.MissionBriefs[number])]);
+                                           [new Section(Constants.MissionBriefs[number])],
+                                           VisibilityRules: [$"MissionsRequired,{GetMissionCharacter(number)}Playable"]);
         await FileWriter.WriteFile(JsonSerializer.Serialize(missions, Constants.JsonOptions),
                                    "missions.json",
                                    "locations");
