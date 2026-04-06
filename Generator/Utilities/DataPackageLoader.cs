655348e8c2f71674c79bcde1fc0739e1e2abc0a6
Dave Morley <raceprouk@hotmail.co.uk>
2026-04-06 15:11:00 +0100

    L113 - Switch to hardcoded filename (checksum should never change)

--- Generator/Utilities/DataPackageLoader.cs
+++ Generator/Utilities/DataPackageLoader.cs
@@ -8,33 +8,8 @@ internal static class DataPackageLoader
     internal static async Task<DataPackage?> LoadDataPackage()
     {
         var appdataLocal = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        var dpLocation = Path.Combine(appdataLocal, "Archipelago", "Cache", "datapackage", "Sonic Adventure DX");
-        var files = Directory.GetFiles(dpLocation);
-        var dpFilePath = files.Length switch
-        {
-            1 => files[0],
-            > 1 => ChooseFile(files),
-            _ => throw new FileNotFoundException("Cannot find datapackage")
-        };
+        var dpFilePath = Path.Combine(appdataLocal, "Archipelago", "Cache", "datapackage", "Sonic Adventure DX", "7d5f6018d7efb5811273ecc815fe29de274f5eb1.json");
         using var dpFile = File.OpenRead(dpFilePath);
         return await JsonSerializer.DeserializeAsync<DataPackage>(dpFile, Constants.JsonOptions);
-    }
-
-    private static string ChooseFile(string[] files)
-    {
-        while (true)
-        {
-            Console.WriteLine("Choose a datapackage to load:");
-            for (var i = 0; i < files.Length; i++)
-                Console.WriteLine($"{i} - {files[i]} (last updated {File.GetLastWriteTime(files[i])})");
-
-            var input = Console.ReadLine();
-            if (int.TryParse(input, out var id) && id >= 0 && id < files.Length)
-                return files[id];
-            else
-            {
-                Console.WriteLine("Invalid selection");
-            }
-        }
     }
 }
