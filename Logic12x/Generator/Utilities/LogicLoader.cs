1ab31454628b4f7d56ff9410af2335350219e799
Dave Morley <raceprouk@hotmail.co.uk>
2026-04-14 20:30:38 +0100

    Migrated to Google Sheets API from SheetToObjects

--- Logic12x/Generator/Utilities/LogicLoader.cs
+++ Logic12x/Generator/Utilities/LogicLoader.cs
@@ -1,10 +1,10 @@
 ﻿using System.Text.RegularExpressions;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
 using Humanizer;
 using Microsoft.Extensions.Configuration;
 using RPS.SADX.PopTracker.Generator.Models.Logic;
-using SheetToObjects.Adapters.GoogleSheets;
-using SheetToObjects.Lib;
-using SheetToObjects.Lib.FluentConfiguration;
 
 namespace RPS.SADX.PopTracker.Generator.Utilities;
 
@@ -18,91 +18,128 @@ internal static partial class LogicLoader
         ApiKey = config["GoogleApiKey"]!;
     }
 
-    internal static IAsyncEnumerable<Connection> LoadForConnections()
-        => LoadFor<Connection>("B3:J573", _ =>
-            _.MapColumn(_ => _.WithColumnIndex(0).IsRequired().MapTo(_ => _.Character))
-             .MapColumn(_ => _.WithColumnIndex(1).IsRequired().MapTo(_ => _.AreaFrom))
-             .MapColumn(_ => _.WithColumnIndex(2).IsRequired().MapTo(_ => _.AreaTo))
-             .MapColumn(_ => _.WithColumnIndex(3).MapTo(_ => _.Tag))
-             .MapCommonColumns(ParseKeyItemLogicRules));
-
-    internal static IAsyncEnumerable<Capsule> LoadForCapsule()
-        => LoadFor<Capsule>("B1534:J2226", _ =>
-            _.MapColumn(_ => _.WithColumnIndex(0).IsRequired().MapTo(_ => _.Level))
-             .MapColumn(_ => _.WithColumnIndex(1).IsRequired().MapTo(_ => _.Character))
-             .MapColumn(_ => _.WithColumnIndex(2).IsRequired().MapTo(_ => _.Type))
-             .MapColumn(_ => _.WithColumnIndex(3).IsRequired().MapTo(_ => _.Number))
-             .MapCommonColumns(ParseKeyItemLogicRules));
-
-    internal static IAsyncEnumerable<Enemy> LoadForEnemy()
-        => LoadFor<Enemy>("B822:J1531", _ =>
-            _.MapColumn(_ => _.WithColumnIndex(0).IsRequired().MapTo(_ => _.Level))
-             .MapColumn(_ => _.WithColumnIndex(1).IsRequired().MapTo(_ => _.Character))
-             .MapColumn(_ => _.WithColumnIndex(2).IsRequired().MapTo(_ => _.Type))
-             .MapColumn(_ => _.WithColumnIndex(3).IsRequired().MapTo(_ => _.Number))
-             .MapCommonColumns(ParseKeyItemLogicRules));
-
-    internal static IAsyncEnumerable<FieldEmblem> LoadForFieldEmblem()
-        => LoadFor<FieldEmblem>("B728:J740", _ =>
-            _.MapColumn(_ => _.WithColumnIndex(0).IsRequired().ParseValueUsing(ParseAreaName).MapTo(_ => _.Area))
-             .MapColumn(_ => _.WithColumnIndex(1).IsRequired().MapTo(_ => _.Name))
-             .MapCommonColumns(ParseEmblemLogicRules));
-
-    internal static IAsyncEnumerable<Fish> LoadForFish()
-        => LoadFor<Fish>("B2229:J2252", _ =>
-            _.MapColumn(_ => _.WithColumnIndex(0).IsRequired().MapTo(_ => _.Level))
-             .MapColumn(_ => _.WithColumnIndex(1).IsRequired().MapTo(_ => _.Type))
-             .MapCommonColumns(ParseKeyItemLogicRules));
-
-    internal static IAsyncEnumerable<LevelMission> LoadForLevelMission()
-        => LoadFor<LevelMission>("B577:J705", _ =>
-            _.MapColumn(_ => _.WithColumnIndex(0).IsRequired().ParseValueUsing(ParseAreaName).MapTo(_ => _.Level))
-             .MapColumn(_ => _.WithColumnIndex(1).IsRequired().MapTo(_ => _.Character))
-             .MapColumn(_ => _.WithColumnIndex(2).IsRequired().MapTo(_ => _.Mission))
-             .MapCommonColumns(ParseKeyItemLogicRules));
-
-    internal static IAsyncEnumerable<Mission> LoadForMission()
-        => LoadFor<Mission>("B743:J860", _ =>
-            _.MapColumn(_ => _.WithColumnIndex(0).IsRequired().MapTo(_ => _.CardArea))
-             .MapColumn(_ => _.WithColumnIndex(1).IsRequired().MapTo(_ => _.ObjectiveArea))
-             .MapColumn(_ => _.WithColumnIndex(2).IsRequired().MapTo(_ => _.Character))
-             .MapColumn(_ => _.WithColumnIndex(3).IsRequired().MapTo(_ => _.Number))
-             .MapCommonColumns(ParseKeyItemLogicRules));
-
-    internal static IAsyncEnumerable<UpgradeItem> LoadForUpgradeItem()
-         => LoadFor<UpgradeItem>("B708:J725", _ =>
-             _.MapColumn(_ => _.WithColumnIndex(0).IsRequired().ParseValueUsing(ParseAreaName).MapTo(_ => _.Area))
-              .MapColumn(_ => _.WithColumnIndex(1).IsRequired().MapTo(_ => _.Character))
-              .MapColumn(_ => _.WithColumnIndex(2).IsRequired().MapTo(_ => _.Upgrade))
-              .MapCommonColumns(ParseKeyItemLogicRules));
-
-    private static MappingConfigBuilder<T> MapCommonColumns<T>(this MappingConfigBuilder<T> builder, Func<string, object> valueParser)
-        where T : LogicSpecification
-        => builder.MapColumn(_ => _.WithColumnIndex(4).ParseValueUsing(valueParser).MapTo(_ => _.NormalLogic))
-                  .MapColumn(_ => _.WithColumnIndex(5).ParseValueUsing(valueParser).MapTo(_ => _.HardLogic))
-                  .MapColumn(_ => _.WithColumnIndex(6).ParseValueUsing(valueParser).MapTo(_ => _.ExpertDCLogic))
-                  .MapColumn(_ => _.WithColumnIndex(7).ParseValueUsing(valueParser).MapTo(_ => _.ExpertDXLogic))
-                  .MapColumn(_ => _.WithColumnIndex(8).ParseValueUsing(valueParser).MapTo(_ => _.ExpertDXPlusLogic));
-
-    private static async IAsyncEnumerable<T> LoadFor<T>(string range,
-                                                        Func<MappingConfigBuilder<T>, MappingConfigBuilder<T>> mapping) where T : new()
+    internal static IAsyncEnumerable<Connection> LoadForConnections() => LoadFor<Connection>("B3:J573", row => new()
     {
-        var adapter = new GoogleSheetAdapter();
-        var sheet = await adapter.GetAsync("1XTdY4A6WUXBDqCwr2n7fuOTlDFJUs5o82pKRkiQ5vic",
-                                           $"Logic (1.2.1)!{range}",
-                                           ApiKey);
-        var mapper = new SheetMapper().AddConfigFor(mapping);
-        var mapped = mapper.Map<T>(sheet);
-        foreach (var model in mapped.ParsedModels)
+        Character = row.Values[0].EffectiveValue.StringValue,
+        AreaFrom = ParseAreaName(row.Values[1].EffectiveValue?.StringValue ?? string.Empty),
+        AreaTo = ParseAreaName(row.Values[2].EffectiveValue.StringValue),
+        Tag = row.Values[3].EffectiveValue?.StringValue,
+        NormalLogic = ParseKeyItemLogicRules(row.Values[4].EffectiveValue?.StringValue),
+        HardLogic = ParseKeyItemLogicRules(row.Values[5].EffectiveValue?.StringValue),
+        ExpertDCLogic = ParseKeyItemLogicRules(row.Values[6].EffectiveValue?.StringValue),
+        ExpertDXLogic = ParseKeyItemLogicRules(row.Values[7].EffectiveValue?.StringValue),
+        ExpertDXPlusLogic = ParseKeyItemLogicRules(row.Values[8].EffectiveValue?.StringValue),
+    });
+
+    internal static IAsyncEnumerable<Capsule> LoadForCapsule() => LoadFor<Capsule>("B1534:J2226", row => new()
+    {
+        Level = ParseAreaName(row.Values[0].EffectiveValue.StringValue),
+        Character = row.Values[1].EffectiveValue.StringValue,
+        Type = row.Values[2].EffectiveValue.StringValue,
+        Number = Convert.ToInt32(row.Values[3].EffectiveValue.NumberValue),
+        NormalLogic = ParseKeyItemLogicRules(row.Values[4].EffectiveValue?.StringValue),
+        HardLogic = ParseKeyItemLogicRules(row.Values[5].EffectiveValue?.StringValue),
+        ExpertDCLogic = ParseKeyItemLogicRules(row.Values[6].EffectiveValue?.StringValue),
+        ExpertDXLogic = ParseKeyItemLogicRules(row.Values[7].EffectiveValue?.StringValue),
+        ExpertDXPlusLogic = ParseKeyItemLogicRules(row.Values[8].EffectiveValue?.StringValue),
+    });
+
+    internal static IAsyncEnumerable<Enemy> LoadForEnemy() => LoadFor<Enemy>("B822:J1531", row => new()
+    {
+        Level = ParseAreaName(row.Values[0].EffectiveValue.StringValue),
+        Character = row.Values[1].EffectiveValue.StringValue,
+        Type = row.Values[2].EffectiveValue.StringValue,
+        Number = Convert.ToInt32(row.Values[3].EffectiveValue.NumberValue),
+        NormalLogic = ParseKeyItemLogicRules(row.Values[4].EffectiveValue?.StringValue),
+        HardLogic = ParseKeyItemLogicRules(row.Values[5].EffectiveValue?.StringValue),
+        ExpertDCLogic = ParseKeyItemLogicRules(row.Values[6].EffectiveValue?.StringValue),
+        ExpertDXLogic = ParseKeyItemLogicRules(row.Values[7].EffectiveValue?.StringValue),
+        ExpertDXPlusLogic = ParseKeyItemLogicRules(row.Values[8].EffectiveValue?.StringValue),
+    });
+
+    internal static IAsyncEnumerable<FieldEmblem> LoadForFieldEmblem() => LoadFor<FieldEmblem>("B728:J740", row => new()
+    {
+        Area = ParseAreaName(row.Values[0].EffectiveValue.StringValue),
+        Name = row.Values[1].EffectiveValue.StringValue,
+        NormalLogic = ParseEmblemLogicRules(row.Values[4].EffectiveValue?.StringValue),
+        HardLogic = ParseEmblemLogicRules(row.Values[5].EffectiveValue?.StringValue),
+        ExpertDCLogic = ParseEmblemLogicRules(row.Values[6].EffectiveValue?.StringValue),
+        ExpertDXLogic = ParseEmblemLogicRules(row.Values[7].EffectiveValue?.StringValue),
+        ExpertDXPlusLogic = ParseEmblemLogicRules(row.Values[8].EffectiveValue?.StringValue),
+    });
+
+    internal static IAsyncEnumerable<Fish> LoadForFish() => LoadFor<Fish>("B2229:J2252", row => new()
+    {
+        Level = ParseAreaName(row.Values[0].EffectiveValue.StringValue),
+        Type = row.Values[1].EffectiveValue.StringValue,
+        NormalLogic = ParseKeyItemLogicRules(row.Values[4].EffectiveValue?.StringValue),
+        HardLogic = ParseKeyItemLogicRules(row.Values[5].EffectiveValue?.StringValue),
+        ExpertDCLogic = ParseKeyItemLogicRules(row.Values[6].EffectiveValue?.StringValue),
+        ExpertDXLogic = ParseKeyItemLogicRules(row.Values[7].EffectiveValue?.StringValue),
+        ExpertDXPlusLogic = ParseKeyItemLogicRules(row.Values[8].EffectiveValue?.StringValue),
+    });
+
+    internal static IAsyncEnumerable<LevelMission> LoadForLevelMission() => LoadFor<LevelMission>("B577:J705", row => new()
+    {
+        Level = ParseAreaName(row.Values[0].EffectiveValue.StringValue),
+        Character = row.Values[1].EffectiveValue.StringValue,
+        Mission = row.Values[2].EffectiveValue.StringValue,
+        NormalLogic = ParseKeyItemLogicRules(row.Values[4].EffectiveValue?.StringValue),
+        HardLogic = ParseKeyItemLogicRules(row.Values[5].EffectiveValue?.StringValue),
+        ExpertDCLogic = ParseKeyItemLogicRules(row.Values[6].EffectiveValue?.StringValue),
+        ExpertDXLogic = ParseKeyItemLogicRules(row.Values[7].EffectiveValue?.StringValue),
+        ExpertDXPlusLogic = ParseKeyItemLogicRules(row.Values[8].EffectiveValue?.StringValue),
+    });
+
+    internal static IAsyncEnumerable<Mission> LoadForMission() => LoadFor<Mission>("B743:J803", row => new()
+    {
+        CardArea = ParseAreaName(row.Values[0].EffectiveValue.StringValue),
+        ObjectiveArea = ParseAreaName(row.Values[1].EffectiveValue.StringValue),
+        Character = row.Values[2].EffectiveValue.StringValue,
+        Number = Convert.ToInt32(row.Values[3].EffectiveValue.NumberValue),
+        NormalLogic = ParseKeyItemLogicRules(row.Values[4].EffectiveValue?.StringValue),
+        HardLogic = ParseKeyItemLogicRules(row.Values[5].EffectiveValue?.StringValue),
+        ExpertDCLogic = ParseKeyItemLogicRules(row.Values[6].EffectiveValue?.StringValue),
+        ExpertDXLogic = ParseKeyItemLogicRules(row.Values[7].EffectiveValue?.StringValue),
+        ExpertDXPlusLogic = ParseKeyItemLogicRules(row.Values[8].EffectiveValue?.StringValue),
+    });
+
+    internal static IAsyncEnumerable<UpgradeItem> LoadForUpgradeItem() => LoadFor<UpgradeItem>("B708:J725", row => new()
+    {
+        Area = ParseAreaName(row.Values[0].EffectiveValue.StringValue),
+        Character = row.Values[1].EffectiveValue.StringValue,
+        Upgrade = row.Values[2].EffectiveValue.StringValue,
+        NormalLogic = ParseKeyItemLogicRules(row.Values[4].EffectiveValue?.StringValue),
+        HardLogic = ParseKeyItemLogicRules(row.Values[5].EffectiveValue?.StringValue),
+        ExpertDCLogic = ParseKeyItemLogicRules(row.Values[6].EffectiveValue?.StringValue),
+        ExpertDXLogic = ParseKeyItemLogicRules(row.Values[7].EffectiveValue?.StringValue),
+        ExpertDXPlusLogic = ParseKeyItemLogicRules(row.Values[8].EffectiveValue?.StringValue),
+    });
+
+    private static async IAsyncEnumerable<T> LoadFor<T>(string range, Func<RowData, T> mapper) where T : new()
+    {
+        var service = new SheetsService(new BaseClientService.Initializer
         {
-            yield return model.Value;
+            ApplicationName = "SADX PopTracker Generator",
+            ApiKey = ApiKey
+        });
+        var request = service.Spreadsheets.Get("1XTdY4A6WUXBDqCwr2n7fuOTlDFJUs5o82pKRkiQ5vic");
+        request.IncludeGridData = true;
+        request.Ranges = $"Logic (1.2.1)!{range}";
+
+        var response = await request.ExecuteAsync();
+        foreach (var row in response.Sheets[0].Data[0].RowData.Skip(1))
+        {
+            yield return mapper(row);
         }
     }
 
     private static string ParseAreaName(string s) => s.Pascalize();
 
-    private static LogicRules ParseKeyItemLogicRules(string s)
+    private static LogicRules ParseKeyItemLogicRules(string? s)
     {
+        if (s is null) return [];
+
         s = Common.RemoveWhitespace(s).Pascalize().Replace("AllLures", "Lure1,Lure2,Lure3,Lure4");
 
         var rules = new LogicRules();
@@ -115,8 +152,10 @@ internal static partial class LogicLoader
         return rules;
     }
 
-    private static LogicRules ParseEmblemLogicRules(string s)
+    private static LogicRules ParseEmblemLogicRules(string? s)
     {
+        if (s is null) return [];
+
         s = Common.RemoveWhitespace(s);
 
         var rules = new LogicRules();
