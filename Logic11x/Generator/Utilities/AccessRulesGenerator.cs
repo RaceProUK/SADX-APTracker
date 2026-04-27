04ce6ea9dc4ab2548372957a5b20b97e688f26c8
Dave Morley <raceprouk@hotmail.co.uk>
2026-04-27 19:23:05 +0100

    Refactor to simplify cognitive complexity

--- Logic11x/Generator/Utilities/AccessRulesGenerator.cs
+++ Logic11x/Generator/Utilities/AccessRulesGenerator.cs
@@ -2,6 +2,7 @@
 using QuikGraph;
 using QuikGraph.Algorithms;
 using RPS.SADX.PopTracker.Generator.Models.Logic;
+using System.Linq;
 
 namespace RPS.SADX.PopTracker.Generator.Utilities;
 
@@ -33,51 +34,50 @@ internal static class AccessRulesGenerator
                       from areaTo in Areas
                       where !string.Equals(areaFrom, areaTo, StringComparison.OrdinalIgnoreCase)
                       from logicLevel in Enumerable.Range(0, 5)
-                      let rule = MakeLogicRule(character, areaFrom, areaTo, logicLevel)
+                      let rule = MakeLogicRule(graphs, character, areaFrom, areaTo, logicLevel)
                       select $"    [\"{character} - {areaFrom} - {areaTo} - {logicLevel}\"] = function() return {rule} end,";
         await FileWriter.WriteFile(string.Join(Environment.NewLine, ["ReachRules = {", .. entries, "}"]),
                                                "reachRules.lua",
                                                "scripts",
                                                "logic");
+    }
 
-        string MakeLogicRule(string character, string from, string to, int logicLevel)
+    private static string MakeLogicRule(Dictionary<string, BidirectionalGraph<string, AreaToArea>> graphs, string character, string from, string to, int logicLevel)
+    {
+        if (!graphs.TryGetValue(character, out var graph))
+            return "false";
+
+        var itemSets = new List<HashSet<string>>();
+        foreach (var path in graph.RankedShortestPathHoffmanPavley(_ => 1, @from, to, 4).OrderBy(_ => _.Count()))
         {
-            if (!graphs.TryGetValue(character, out var graph))
-                return "false";
-
-            var itemSets = new List<HashSet<string>>();
-            foreach (var path in graph.RankedShortestPathHoffmanPavley(_ => 1, @from, to, 4).OrderBy(_ => _.Count()))
+            var steps = path.Select(_ => logicLevel switch
             {
-                var steps = path.Select(_ => logicLevel switch
-                {
-                    0 => _.NormalLogic,
-                    1 => _.HardLogic,
-                    2 => _.ExpertDCLogic,
-                    3 => _.ExpertDXLogic,
-                    4 => _.ExpertDXPlusLogic,
-                    _ => []
-                });
-                if (steps.All(_ => !_.Any()))
-                {
-                    // If no item is needed for a path, then discard all item sets and move on
-                    itemSets.Clear();
-                    break;
-                }
-                else
-                {
-                    // Build an item set for each chain
-                    foreach (var chain in CartesianProduct(steps))
-                    {
-                        var items = chain.SelectMany(_ => _);
-                        if (items.Any()) //But only if said chain has items
-                            itemSets.Add([.. items]);
-                    }
-                }
+                0 => _.NormalLogic,
+                1 => _.HardLogic,
+                2 => _.ExpertDCLogic,
+                3 => _.ExpertDXLogic,
+                4 => _.ExpertDXPlusLogic,
+                _ => []
+            });
+            if (steps.All(_ => !_.Any()))
+            {
+                // If no item is needed for a path, then discard all item sets and move on
+                itemSets.Clear();
+                break;
             }
-            var rules = itemSets.Distinct(HashSet<string>.CreateSetComparer())
-                                .Select(_ => string.Join(" and ", _.Select(_ => $"HasItem(\"{_}\")")));
-            return rules.Any() ? string.Join(" or ", rules.OrderBy(_ => _.Length)) : "true";
+            else
+            {
+                // Build an item set for each chain only if said chain has items
+                itemSets.AddRange(from chain in CartesianProduct(steps)
+                                  let items = chain.SelectMany(_ => _)
+                                  where items.Any()
+                                  select new HashSet<string>(items));
+            }
         }
+
+        var rules = itemSets.Distinct(HashSet<string>.CreateSetComparer())
+                            .Select(_ => string.Join(" and ", _.Select(_ => $"HasItem(\"{_}\")")));
+        return rules.Any() ? string.Join(" or ", rules.OrderBy(_ => _.Length)) : "true";
     }
 
     private static async Task GenerateLevelAccessRules()
